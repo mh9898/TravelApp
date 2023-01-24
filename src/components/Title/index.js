356e60e69f2d4176263983422a34dd72fcd16a55
MH <mh9898@gmail.com>
2023-01-24 14:56:05 +0200

    Title Component

---
+++ src/components/Title/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Title = ({text}) => {
+  return <Text>{text}</Text>;
+};
+
+export default Title;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from '../../components/Title';
+import {View} from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Title text="Title prop from home" />
+    </View>
+  );
+};
+
+export default Home;
