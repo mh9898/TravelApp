889e9f1b97eef9f49a276fbccb8b5295a8c67c07
MH <mh9898@gmail.com>
2023-01-26 15:47:32 +0200

    clean list

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -2,14 +2,6 @@ import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const Categories = ({categories}) => {
-  const categories_ = [
-    {name: 'Beaches', id: 1},
-    {name: 'Mountains', id: 2},
-    {name: 'Cities', id: 3},
-    {name: 'Adventure', id: 4},
-    {name: 'Nature', id: 5},
-  ];
-
   return (
     <FlatList data={categories} renderItem={({item}) => <Text>{item}</Text>} />
   );
